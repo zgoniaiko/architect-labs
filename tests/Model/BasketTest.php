7013419b444c64ae707d0383831a16519ac14443
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2019-04-18 16:19:07 +0300

    Add test for add 2 different products

--- tests/Model/BasketTest.php
+++ tests/Model/BasketTest.php
@@ -11,17 +11,31 @@ class BasketTest extends TestCase
     private $basket;
 
     private $redWidget;
+    private $greenWidget;
+    private $blueWidget;
 
     protected function setUp()
     {
         parent::setUp();
 
-        $this->redWidget = new Product();
-        $this->redWidget
+        $this->redWidget = (new Product())
             ->setName('Red Widget')
             ->setCode('R01')
             ->setPrice(32.95)
         ;
+
+        $this->greenWidget = (new Product())
+            ->setName('Green Widget')
+            ->setCode('G01')
+            ->setPrice(24.95)
+        ;
+
+        $this->blueWidget = (new Product())
+            ->setName('Blue Widget')
+            ->setCode('B01')
+            ->setPrice(7.95)
+        ;
+
 
         $this->basket = new Basket();
     }
@@ -37,6 +51,15 @@ class BasketTest extends TestCase
 
         $this->basket->add($this->redWidget->getCode());
         $this->assertEquals(1, $this->basket->countProducts());
+    }
+
+    public function testAddTwoDifferentProducts()
+    {
+        $this->basket->add($this->greenWidget->getCode());
+        $this->assertEquals(1, $this->basket->countProducts());
+
+        $this->basket->add($this->blueWidget->getCode());
+        $this->assertEquals(2, $this->basket->countProducts());
     }
 
     public function testAddSameProductTwice()
